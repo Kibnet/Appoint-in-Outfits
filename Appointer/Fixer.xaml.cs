ff2e8f15f67c9b331fb132c2ac4c82c1264e737c
Kibnet <Kibnet@hotmail.com>
2015-01-30 14:48:20 +1000

    +Добавил окно разрешения конфликтов +Для 2 числа сделал союз "со" +Сделал нормальный поиск на основе класса Person -Исправил жаргонизмы

---
+++ Appointer/Fixer.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Appointer
+{
+	/// <summary>
+	/// Логика взаимодействия для Fixer.xaml
+	/// </summary>
+	public partial class Fixer : Window
+	{
+		public Fixer()
+		{
+			InitializeComponent();
+		}
+
+		private void PersonBox_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			Close();
+		}
+	}
+}
--- Appointer/MainWindow.xaml.cs
+++ Appointer/MainWindow.xaml.cs
@@ -1,20 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using Appointer.Properties;
 
 namespace Appointer
 {
+
+	public class Person
+	{
+		public string Family;
+		public string Name;
+		public string FatherName;
+		public string All;
+		public bool Regular;
+
+		public static Person CreatePerson(string personinfo, bool regular = true)
+		{
+			var person = new Person();
+			var match = Regex.Match(personinfo, @"(?<fam>[А-Я]+)\s+(?<nam>[А-Я])\.(?<fna>[А-Я])\.");
+			if (!match.Success)
+			{
+				match = Regex.Match(personinfo, @"(?<fam>[А-Я]+)\s+(?<nam>[А-Яа-я]+)\s+(?<fna>[А-Яа-я]+),");
+			}
+			if (!match.Success) return null;
+			person.Family = match.Groups["fam"].Value;
+			person.Name = match.Groups["nam"].Value;
+			person.FatherName = match.Groups["fna"].Value;
+			person.All = personinfo;
+			person.Regular = regular;
+			return person;
+		}
+
+		public override string ToString()
+		{
+			return All;
+		}
+	}
+
 	/// <summary>
 	/// Логика взаимодействия для MainWindow.xaml
 	/// </summary>
@@ -22,7 +47,9 @@ namespace Appointer
 	{
 		public MainWindow()
 		{
-			InitializeComponent();
+			InitializeComponent(); 
+			var ass = Assembly.GetExecutingAssembly().FullName;
+			Title += " " + ass.Split(',')[1].Split('=')[1];
 		}
 
 		private void Appoint(object sender, RoutedEventArgs e)
@@ -32,7 +59,10 @@ namespace Appointer
 				var persons = Settings.Default.Persons.Cast<string>().ToList();
 				var comendas = Settings.Default.Comendas.Cast<string>().ToList();
 				var outfits = Settings.Default.Outfits.Cast<string>().ToList();
-				
+
+				var all = (from string personinfo in Settings.Default.Persons select Person.CreatePerson(personinfo, true)).ToList();
+				all.AddRange(from string personinfo in Settings.Default.Comendas select Person.CreatePerson(personinfo, false));
+
 				var appointpers = new StringBuilder("1. В наряд охраны назначить:\r\n");
 				var appointcoms = new StringBuilder("2. Заступить на службу:\r\n");
 
@@ -44,38 +74,61 @@ namespace Appointer
 					foreach (var line in lines)
 					{
 						var fields = line.Replace("\r", "").Split('\t');
-						if (fields.Length == outfits.Count + 1)
+						if (fields.Length <= outfits.Count + 1)
 						{
 							var day = SelectCalendar.DisplayDate.AddDays(-SelectCalendar.DisplayDate.Day);
 							var dd = 1;
 							int.TryParse(fields[0], out dd);
 							day = day.AddDays(dd);
 							var fdate = day.Month == day.AddDays(1).Month ? day.Day.ToString() : (day.Year == day.AddDays(1).Year ? day.ToString("d MMMM") : day.ToString("d MMMM yyyy")+" г.");
-							var date = string.Format("{0}) с {1} на {2} г.:", lett, fdate, day.AddDays(1).ToString("d MMMM yyyy"));
+							var unity = day.Day == 2? "со" : "с";
+							var date = string.Format("{0}) {3} {1} на {2} г.:", lett, fdate, day.AddDays(1).ToString("d MMMM yyyy"),unity);
 							appointpers.AppendLine(date);
 							appointcoms.AppendLine(date);
 
 							for (int i = 1; i < fields.Length; i++)
 							{
 								var field = fields[i];
-								var inic = field.Split(' ').LastOrDefault();
-								var pinic = persons.Where(s => inic != null && s.Contains(inic)).ToArray();
-								var cinic = comendas.Where(s => inic != null && s.Contains(inic)).ToArray();
-								while (field.Length > 0)
+								if (string.IsNullOrWhiteSpace(field))
 								{
-									var pers = pinic.Where(s => s.Contains(field)).ToArray();
-									var coms = cinic.Where(s => s.Contains(field)).ToArray();
-									if (pers.Any())
+									continue;
+								}
+								var personinfo = Person.CreatePerson(field);
+								while (personinfo != null && personinfo.Family.Length > 0)
+								{
+									var candidates =
+										all.Where(
+											person =>
+												person.Family.StartsWith(personinfo.Family) && 
+												person.Name.StartsWith(personinfo.Name) &&
+												person.FatherName.StartsWith(personinfo.FatherName)).ToArray();
+									if (candidates.Length == 1)
 									{
-										appointpers.AppendLine("- " + outfits[i - 1] + " – " + pers.FirstOrDefault());
+										var add = candidates[0];
+										if (add.Regular)
+											appointpers.AppendLine("- " + outfits[i - 1] + " – " + add.All);
+										else
+											appointcoms.AppendLine("- " + outfits[i - 1] + " – " + add.All);
 										break;
 									}
-									if (coms.Any())
+									if (candidates.Length > 1)
 									{
-										appointcoms.AppendLine("- " + outfits[i - 1] + " – " + coms.FirstOrDefault());
-										break;
+										var fixer = new Fixer();
+										fixer.DateBlock.Text = date;
+										fixer.NarBlock.Text = outfits[i - 1];
+										fixer.PersonBox.ItemsSource = candidates;
+										fixer.ShowDialog();
+										if (fixer.PersonBox.SelectedItem != null)
+										{
+											var add = (Person)(fixer.PersonBox.SelectedItem);
+											if (add.Regular)
+												appointpers.AppendLine("- " + outfits[i - 1] + " – " + add.All);
+											else
+												appointcoms.AppendLine("- " + outfits[i - 1] + " – " + add.All);
+											break;
+										}
 									}
-									field = field.Remove(field.Length - 1);
+									personinfo.Family = personinfo.Family.Remove(personinfo.Family.Length - 1);
 								}
 							}
 						}
@@ -97,6 +150,7 @@ namespace Appointer
 
 		private void SetOutfits(object sender, RoutedEventArgs e)
 		{
+			StatusLabel.Text = "Скопируйте часть графика в буфер обмена и нажмите кнопку \"Назначить\"";
 			var setter = new ConfigList(Settings.Default.Outfits);
 			setter.Description.Text = "Названия нарядов в приказе, в том же порядке что и в графике нарядов (отвечает на вопрос 'Кем заступает?')";
 			setter.ShowDialog();
@@ -109,6 +163,7 @@ namespace Appointer
 
 		private void SetComendas(object sender, RoutedEventArgs e)
 		{
+			StatusLabel.Text = "Скопируйте часть графика в буфер обмена и нажмите кнопку \"Назначить\"";
 			var setter = new ConfigList(Settings.Default.Comendas);
 			setter.Description.Text = "Сотрудники комендантского отделения в формате - \nзвание ФАМИЛИЯ ИНИЦИАЛЫ, должность (отвечает на вопрос 'Кому заступить на службу?')";
 			setter.ShowDialog();
@@ -121,6 +176,7 @@ namespace Appointer
 
 		private void SetPersons(object sender, RoutedEventArgs e)
 		{
+			StatusLabel.Text = "Скопируйте часть графика в буфер обмена и нажмите кнопку \"Назначить\"";
 			var setter = new ConfigList(Settings.Default.Persons);
 			setter.Description.Text = "Сотрудники не из комендантского отделения в формате - \nзвание ФАМИЛИЯ ИНИЦИАЛЫ, должность (отвечает на вопрос 'Кого назначить в наряд?')";
 			setter.ShowDialog();
